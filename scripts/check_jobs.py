a55e4ca9223d812a835f3ef63eda78d9a98680ca
harshmittal172-gif <harshmittal172@gmail.com>
2026-08-13 10:59:11 +0100

    Add new keywords for job checking

--- scripts/check_jobs.py
+++ scripts/check_jobs.py
@@ -41,7 +41,13 @@ COMPANIES = [
     "Walmart",
 ]
 
-KEYWORDS = [
+KEYWORDS = [ 
+    "Graduate Account Manager",
+    "Account Manager Graduate Scheme",
+    "Graduate Scheme Account Management",
+    "Entry Level Account Manager",
+    "Account Manager New Graduate",
+    "Early Careers Account Manager"
     "Account Manager",
     "Sales",
     "Marketing",
